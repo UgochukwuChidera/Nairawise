30b1bd287b99f1e0489ff62f71eb61c1be60a5fc
Chidera Ugochukwu <ugochukwuc762@gmail.com>
2025-08-28 12:03:46 +0000

    there should only be one close button in there I dont knwo why there are

--- src/components/blog/create-post-form.tsx
+++ src/components/blog/create-post-form.tsx
@@ -15,13 +15,11 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogFooter,
-  DialogClose,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { EditorToolbar } from './editor-toolbar'
 import { ScrollArea } from '../ui/scroll-area'
-import { X } from 'lucide-react'
 import Image from '@tiptap/extension-image'
 import Placeholder from '@tiptap/extension-placeholder'
 
@@ -62,10 +60,6 @@ export function CreatePostForm() {
           <DialogDescription>
             Fill out the details below and write your content.
           </DialogDescription>
-           <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-                <X className="h-4 w-4" />
-                <span className="sr-only">Close</span>
-            </DialogClose>
         </DialogHeader>
 
         <div className="flex-1 overflow-hidden">
