40f2b0097054d81a2379d6b359a69047eb3c7e66
Chidera Ugochukwu <ugochukwuc762@gmail.com>
2025-08-28 09:30:20 +0000

    I don't mean the logo, I mean the navbar items int the desktop view

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -41,52 +41,89 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 flex h-16 items-center justify-between gap-4 border-b bg-background/95 px-4 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/60 md:px-6">
       {/* Desktop Navigation */}
-      <nav className="hidden flex-1 items-center gap-6 text-lg font-medium md:flex">
-        <Link
-          href="/"
-          className="flex items-center gap-2 text-lg font-semibold"
-        >
-          <Logo />
-        </Link>
-        {navItems.map((item) => (
-          <NavLink key={item.href} href={item.href}>
-            {item.label}
-          </NavLink>
-        ))}
-      </nav>
+      <div className="hidden w-full items-center md:flex">
+        <div className="flex w-1/3">
+          <Link
+            href="/"
+            className="flex items-center gap-2 text-lg font-semibold"
+          >
+            <Logo />
+          </Link>
+        </div>
+        <nav className="flex w-1/3 justify-center gap-6 text-lg font-medium">
+          {navItems.map((item) => (
+            <NavLink key={item.href} href={item.href}>
+              {item.label}
+            </NavLink>
+          ))}
+        </nav>
+        <div className="flex w-1/3 justify-end">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger>
+                  <Sun className="mr-2 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Moon className="absolute mr-2 h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span>Toggle Theme</span>
+                </DropdownMenuSubTrigger>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuSub>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+
 
       {/* Mobile Header */}
       <div className="flex w-full items-center justify-between md:hidden">
-        <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon" className="shrink-0">
-              <Menu className="h-5 w-5" />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left" className="flex w-full flex-col sm:max-w-full">
-            <SheetHeader className="sr-only">
-              <SheetTitle>Navigation Menu</SheetTitle>
-              <SheetDescription>
-                A list of links to navigate the application.
-              </SheetDescription>
-            </SheetHeader>
-            <nav className="grid gap-4 text-base font-medium">
-              <Link
-                href="/"
-                className="mb-4 flex items-center gap-2 text-lg font-semibold"
-                onClick={handleLinkClick}
-              >
-                <Logo />
-              </Link>
-              {navItems.map((item) => (
-                <NavLink key={item.href} href={item.href} onClick={handleLinkClick}>
-                  {item.label}
-                </NavLink>
-              ))}
-            </nav>
-          </SheetContent>
-        </Sheet>
+        <div className="flex items-center justify-start w-1/3">
+          <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="shrink-0">
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex w-full flex-col sm:max-w-full">
+              <SheetHeader className="sr-only">
+                <SheetTitle>Navigation Menu</SheetTitle>
+                <SheetDescription>
+                  A list of links to navigate the application.
+                </SheetDescription>
+              </SheetHeader>
+              <nav className="grid gap-4 text-base font-medium">
+                <Link
+                  href="/"
+                  className="mb-4 flex items-center gap-2 text-lg font-semibold"
+                  onClick={handleLinkClick}
+                >
+                  <Logo />
+                </Link>
+                {navItems.map((item) => (
+                  <NavLink key={item.href} href={item.href} onClick={handleLinkClick}>
+                    {item.label}
+                  </NavLink>
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </div>
 
         <div className="flex flex-1 justify-center">
             <Link href="/" className="inline-flex items-center gap-2 text-lg font-semibold">
@@ -94,7 +131,7 @@ export function Header() {
             </Link>
         </div>
 
-        <div className="flex items-center">
+        <div className="flex items-center justify-end w-1/3">
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button variant="secondary" size="icon" className="rounded-full">
@@ -124,39 +161,6 @@ export function Header() {
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
-      </div>
-
-
-      {/* Desktop User Menu */}
-      <div className="hidden items-center gap-4 md:flex">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="secondary" size="icon" className="rounded-full">
-              <CircleUser className="h-5 w-5" />
-              <span className="sr-only">Toggle user menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>
-                <Sun className="mr-2 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute mr-2 h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span>Toggle Theme</span>
-              </DropdownMenuSubTrigger>
-              <DropdownMenuSubContent>
-                <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuSub>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
     </header>
   )
